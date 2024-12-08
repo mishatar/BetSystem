643fc61a89e7054c6c0c99dfe593ef2a4e20cc9b
mishatar <mishatar2004@gmail.com>
2024-12-09 01:22:17 +0300

    init project & add db connection

---
+++ line_provider/database/config.py
@@ -0,0 +1,25 @@
+import os
+from dataclasses import dataclass
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+@dataclass(frozen=True)
+class BaseSQLConfig:
+    """
+    :param port: Порт для подключения
+    :param host: Адрес БД
+    :param schema: Название схемы
+    :param user: Пользователь БД
+    :param password: Пароль от пользователя БД
+    :param connector: Коннектор для подключения к БД
+    """
+    schema: str = os.getenv("POSTGRES_DB_NAME")
+    host: str = os.getenv("POSTGRES_DB_HOST")
+    port: int = os.getenv("POSTGRES_DB_HOST_PORT")
+    user: str = os.getenv("POSTGRES_DB_USER")
+    password: str = os.getenv("POSTGRES_DB_PASSWORD")
+    connector: str = "postgresql+asyncpg"
+
+db_config = BaseSQLConfig()
